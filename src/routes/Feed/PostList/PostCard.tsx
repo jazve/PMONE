1ff16a8b6c2653332ba111d02cb9ffac7cff149c
jazve <493550274@qq.com>
2024-02-13 15:38:43 +0800

    2

--- src/routes/Feed/PostList/PostCard.tsx
+++ src/routes/Feed/PostList/PostCard.tsx
@@ -63,42 +63,97 @@ export default PostCard
 
 const StyledWrapper = styled(Link)`
   article {
-    display: inline-block;
-    width: auto;
-    padding: 10px 20px;
-    border-radius: 25px;
+    overflow: hidden;
+    position: relative;
+    margin-bottom: 1.5rem;
+    border-radius: 1rem;
     background-color: ${({ theme }) =>
       theme.scheme === "light" ? "white" : theme.colors.gray4};
-    margin-bottom: 10px;
+    transition-property: box-shadow;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 300ms;
+
+    @media (min-width: 768px) {
+      margin-bottom: 2rem;
+    }
 
     :hover {
       box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
         0 4px 6px -2px rgba(0, 0, 0, 0.05);
     }
+    > .category {
+      position: absolute;
+      top: 1rem;
+      left: 1rem;
+      z-index: 10;
+    }
+
+    > .thumbnail {
+      position: relative;
+      width: 100%;
+      background-color: ${({ theme }) => theme.colors.gray2};
+      padding-bottom: 66%;
+
+      @media (min-width: 1024px) {
+        padding-bottom: 50%;
+      }
+    }
     > .content {
-      padding: 0;
+      padding: 1rem;
+
+      &[data-thumb="false"] {
+        padding-top: 3.5rem;
+      }
+      &[data-category="false"] {
+        padding-top: 1.5rem;
+      }
       > .top {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        @media (min-width: 768px) {
+          flex-direction: row;
+          align-items: baseline;
+        }
         h2 {
           margin-bottom: 0.5rem;
           font-size: 1.125rem;
           line-height: 1.75rem;
           font-weight: 500;
+
           cursor: pointer;
+
+          @media (min-width: 768px) {
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+          }
         }
       }
       > .date {
+        display: flex;
         margin-bottom: 1rem;
+        gap: 0.5rem;
+        align-items: center;
         .content {
           font-size: 0.875rem;
           line-height: 1.25rem;
           color: ${({ theme }) => theme.colors.gray10};
+          @media (min-width: 768px) {
+            margin-left: 0;
+          }
         }
       }
       > .summary {
         margin-bottom: 1rem;
         p {
+          display: none;
           line-height: 2rem;
           color: ${({ theme }) => theme.colors.gray11};
+
+          @media (min-width: 768px) {
+            display: block;
+          }
         }
       }
       > .tags {
