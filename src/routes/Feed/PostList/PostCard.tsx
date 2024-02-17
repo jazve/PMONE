34a1292c321e02c9fce39f3ed98e427b67046827
jazve <493550274@qq.com>
2024-02-17 18:11:49 +0800

    1

--- src/routes/Feed/PostList/PostCard.tsx
+++ src/routes/Feed/PostList/PostCard.tsx
@@ -81,8 +81,12 @@ const StyledWrapper = styled(Link)`
     }
 
     :hover {
-      box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-        0 4px 6px -2px rgba(0, 0, 0, 0.05);
+      // box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      //   0 4px 6px -2px rgba(0, 0, 0, 0.05);
+      background-color: ${({ theme }) =>
+        theme.scheme === "light" ? "white" : theme.colors.gray4};
+        border-radius: 1rem;
+        border: none;
     }
     > .category {
       position: absolute;
