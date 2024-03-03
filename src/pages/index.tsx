16df84771919c4e06419dc4f92efedfd23457d74
jazve <493550274@qq.com>
2024-03-03 21:53:31 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import { filterPosts } from "src/libs/utils/notion"
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = filterPosts(await getPosts())
-  await queryClient.prefetchQuery(queryKey.posts(), () => posts)
+  await queryClient.prefetchQuery(queryKey.posts(1), () => posts)
 
   return {
     props: {
