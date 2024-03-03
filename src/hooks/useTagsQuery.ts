61bb11c30f4fa5a9ed5deed9cfc5588e4e4c1eeb
jazve <493550274@qq.com>
2024-03-03 21:50:06 +0800

    2

--- src/hooks/useTagsQuery.ts
+++ src/hooks/useTagsQuery.ts
@@ -2,7 +2,7 @@ import usePostsQuery from "./usePostsQuery"
 import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 
 export const useTagsQuery = () => {
-  const posts = usePostsQuery()
+  const posts = usePostsQuery(1)
   const tags = getAllSelectItemsFromPosts("tags", posts)
 
   return tags
