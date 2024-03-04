452881ac64b58beac11fecf374dca6e583a983bb
jazve <493550274@qq.com>
2024-03-04 12:49:15 +0800

    1

--- src/hooks/useCategoriesQuery.ts
+++ src/hooks/useCategoriesQuery.ts
@@ -2,7 +2,7 @@ import { DEFAULT_CATEGORY } from "src/constants"
 import usePostsQuery from "./usePostsQuery"
 import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 
-const useCategoriesQuery = () => {
+export const useCategoriesQuery = () => {
   const { data: posts, isLoading, isError } = usePostsQuery(1);
   // 确保仅当posts有数据且不在加载或错误状态时调用getAllSelectItemsFromPosts
   const categories = posts && !isLoading && !isError ? getAllSelectItemsFromPosts("category", posts) : [];
