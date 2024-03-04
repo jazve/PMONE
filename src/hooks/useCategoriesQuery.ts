8ab566114aa861ccd3c9977616bb4c3e40cd7eb3
jazve <493550274@qq.com>
2024-03-04 21:10:08 +0800

    1

--- src/hooks/useCategoriesQuery.ts
+++ src/hooks/useCategoriesQuery.ts
@@ -4,8 +4,7 @@ import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 import { CategoryData } from 'src/types';
 
 export const useCategoriesQuery = () => {
-  const { data: posts, isLoading, isError } = usePostsQuery(1);
-  // 确保仅当posts有数据且不在加载或错误状态时调用getAllSelectItemsFromPosts
+  const { data: posts, isLoading, isError } = usePostsQuery(1, 100); // 假设你的API支持这个大的pageSize
   const categories = posts && !isLoading && !isError ? getAllSelectItemsFromPosts("category", posts) as CategoryData : {};
   return {
     [DEFAULT_CATEGORY]: posts.length,
