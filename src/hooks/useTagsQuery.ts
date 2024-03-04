fdf4ef41b9a39ba69f141d1342f8de5344cb9bcd
jazve <493550274@qq.com>
2024-03-04 12:46:50 +0800

    1

--- src/hooks/useTagsQuery.ts
+++ src/hooks/useTagsQuery.ts
@@ -2,8 +2,10 @@ import usePostsQuery from "./usePostsQuery"
 import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 
 export const useTagsQuery = () => {
-  const posts = usePostsQuery(1)
-  const tags = getAllSelectItemsFromPosts("tags", posts)
+  const { data: posts, isLoading, isError } = usePostsQuery(1);
+  // 只有当posts数据可用时才尝试获取tags
+  const tags = !isLoading && !isError ? getAllSelectItemsFromPosts("tags", posts) : [];
 
-  return tags
-}
+  return tags;
+};
+
