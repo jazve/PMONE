cb1fa9da15eed47e224dc013b85b6a402cac2c85
jazve <493550274@qq.com>
2024-03-04 12:57:13 +0800

    1

--- src/routes/Feed/FeedHeader/CategorySelect.tsx
+++ src/routes/Feed/FeedHeader/CategorySelect.tsx
@@ -50,3 +50,43 @@ const CategorySelect: React.FC<Props> = () => {
 }
 
 export default CategorySelect
+
+
+const StyledWrapper = styled.div`
+  position: relative;
+  > .wrapper {
+    display: flex;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    gap: 0.25rem;
+    align-items: center;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+    font-weight: 700;
+    cursor: pointer;
+  }
+  > .content {
+    position: absolute;
+    z-index: 40;
+    padding: 0.25rem;
+    border-radius: 0.75rem;
+    background-color: ${({ theme }) => theme.colors.gray2};
+    color: ${({ theme }) => theme.colors.gray10};
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    > .item {
+      padding: 0.25rem;
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
+      border-radius: 0.75rem;
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+      white-space: nowrap;
+      cursor: pointer;
+
+      :hover {
+        background-color: ${({ theme }) => theme.colors.gray4};
+      }
+    }
+  }
+`
