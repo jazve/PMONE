1742ef5c03b5a28edafff322ee11e2c9e5018e90
jazve <493550274@qq.com>
2024-03-05 13:59:52 +0800

    2

--- src/hooks/useCategoriesQuery.ts
+++ src/hooks/useCategoriesQuery.ts
@@ -4,7 +4,7 @@ import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 import { CategoryData } from 'src/types';
 
 export const useCategoriesQuery = () => {
-  const { data: posts, isLoading, isError } = usePostsQuery(1, 100); // 假设你的API支持这个大的pageSize
+  const { data: posts, isLoading, isError } = usePostsQuery(1, 10); // 假设你的API支持这个大的pageSize
   const categories = posts && !isLoading && !isError ? getAllSelectItemsFromPosts("category", posts) as CategoryData : {};
   return {
     [DEFAULT_CATEGORY]: posts.length,
--- src/hooks/useTagsQuery.ts
+++ src/hooks/useTagsQuery.ts
@@ -2,7 +2,7 @@ import usePostsQuery from "./usePostsQuery"
 import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 
 export const useTagsQuery = () => {
-  const { data: posts, isLoading, isError } = usePostsQuery(1,100);
+  const { data: posts, isLoading, isError } = usePostsQuery(1,10);
   // 只有当posts数据可用时才尝试获取tags
   const tags = !isLoading && !isError ? getAllSelectItemsFromPosts("tags", posts) : [];
 
